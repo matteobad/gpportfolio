b7cc6144f3b0d510dcea359a96d4be5d03a211b4
Matteo Badini <matteo.badini95@gmail.com>
2023-06-21 22:39:57 +0200

    feat: add particles

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -7,6 +7,7 @@ import { FileText, Linkedin, Mail } from "lucide-react";
 
 import { Card } from "@components/card";
 import { Navigation } from "@components/nav";
+import Particles from "@components/particles";
 
 const socials = [
   {
@@ -33,6 +34,10 @@ export default function Example() {
   return (
     <div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
       <Navigation />
+      <Particles
+        className="absolute inset-0 -z-10 animate-fade-in"
+        quantity={100}
+      />
       <div className="container mx-auto flex min-h-screen items-center justify-center px-6">
         <div className="mx-auto mt-10 grid w-full max-w-3xl grid-cols-1 gap-8 lg:gap-16">
           <Card>
